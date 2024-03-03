2c33cdb04a65b6a2dac416ec6d0073d50864663e
alexeisanko <alexeisanko@gmail.com>
2024-03-04 02:22:51 +0300

    realized algorithm three menu

---
+++ uptrader_task/main/templatetags/draw_menu.py
@@ -0,0 +1,28 @@
+from django import template
+from django.core.exceptions import ObjectDoesNotExist
+
+from main.models import MenuItem
+
+register = template.Library()
+
+
+@register.inclusion_tag("menu.html")
+def draw_menu(menu: str = None, item_menu: str = None) -> dict:
+    def get_menu(item_menu: str = None, submenu: list = None) -> list:
+        if item_menu is None:
+            menu = list(items.filter(parent=None))
+        else:
+            menu = list(items.filter(parent__name=item_menu))
+        try:
+            menu.insert(menu.index(submenu[0].parent) + 1, submenu)
+        except (IndexError, TypeError):
+            pass
+        try:
+            return get_menu(items.get(name=item_menu).parent.name, menu)
+        except ObjectDoesNotExist:
+            return menu
+        except AttributeError:
+            return get_menu(submenu=menu)
+
+    items = MenuItem.objects.filter(menu__name=menu)
+    return {'menu': get_menu(item_menu)}
